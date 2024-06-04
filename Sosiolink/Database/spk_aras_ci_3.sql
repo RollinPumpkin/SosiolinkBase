be198ed9b0144ab1969a53316fc3176cb385ee66
RollinPumpkin <septapuma@gmail.com>
2024-06-04 07:30:20 +0700

    update database

--- Sosiolink/Database/spk_aras_ci_3.sql
+++ Sosiolink/Database/spk_aras_ci_3.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 26 Nov 2022 pada 00.14
--- Versi Server: 5.6.26
--- PHP Version: 5.6.12
+-- Generation Time: Jun 04, 2024 at 02:29 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,197 +24,224 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `alternatif`
+-- Table structure for table `alternatif`
 --
 
-CREATE TABLE IF NOT EXISTS `alternatif` (
+CREATE TABLE `alternatif` (
   `id_alternatif` int(11) NOT NULL,
   `nama` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `alternatif`
+-- Dumping data for table `alternatif`
 --
 
 INSERT INTO `alternatif` (`id_alternatif`, `nama`) VALUES
-(16, 'Aceng'),
-(17, 'Aliong'),
-(18, 'Arif'),
-(19, 'Anwar'),
-(20, 'Firman');
+(20, 'Abdul Razzaq Shidiq'),
+(21, 'Afifah Ida Setyo Rini'),
+(22, 'Afina Hasna Maharani'),
+(23, 'Alex Ridwan Ghanilyyu'),
+(24, 'Anggiek Setiawan'),
+(25, 'Ariela Zahwa'),
+(26, 'Arya Naufal Hibrizi'),
+(27, 'Aurel Novalia Arifriandi'),
+(28, 'Cindi Noviani'),
+(29, 'Diva Adelita Margareva');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `hasil`
+-- Table structure for table `hasil`
 --
 
-CREATE TABLE IF NOT EXISTS `hasil` (
+CREATE TABLE `hasil` (
   `id_hasil` int(11) NOT NULL,
   `id_alternatif` int(11) NOT NULL,
   `nilai` float NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `hasil`
+-- Dumping data for table `hasil`
 --
 
 INSERT INTO `hasil` (`id_hasil`, `id_alternatif`, `nilai`) VALUES
-(1, 16, 0.821472),
-(2, 17, 0.689494),
-(3, 18, 0.535621),
-(4, 19, 0.534394),
-(5, 20, 0.738804);
+(1, 20, 0.462659),
+(2, 21, 0.479387),
+(3, 22, 0.442426),
+(4, 23, 0.669598),
+(5, 24, 0.498617),
+(6, 25, 0.729304),
+(7, 26, 0.65768),
+(8, 27, 0.494644),
+(9, 28, 0.664192),
+(10, 29, 0.554488);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kriteria`
+-- Table structure for table `kriteria`
 --
 
-CREATE TABLE IF NOT EXISTS `kriteria` (
+CREATE TABLE `kriteria` (
   `id_kriteria` int(11) NOT NULL,
   `keterangan` varchar(100) NOT NULL,
   `kode_kriteria` varchar(100) NOT NULL,
   `bobot` float NOT NULL,
   `jenis` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `kriteria`
+-- Dumping data for table `kriteria`
 --
 
 INSERT INTO `kriteria` (`id_kriteria`, `keterangan`, `kode_kriteria`, `bobot`, `jenis`) VALUES
-(24, 'Jarak (Km)', 'C1', 0.3, 'Benefit'),
-(25, 'Berapa kali belanja dalam satu bulan', 'C2', 0.3, 'Cost'),
-(26, 'Nominal setiap kali belanja (Jt) ', 'C3', 0.2, 'Benefit'),
-(27, 'Usia', 'C4', 0.15, 'Benefit'),
-(28, 'Berapa item yang dibeli dalam satu bulan', 'C5', 0.05, 'Benefit');
+(24, 'Pendapatan', 'C1', 0.25, 'Cost'),
+(29, 'Kepemilikan Rumah', 'C2', 0.15, 'Cost'),
+(30, 'Tanggungan', 'C3', 0.2, 'Benefit'),
+(31, 'Kondisi Rumah', 'C4', 0.2, 'Cost'),
+(32, 'Kondisi Kesehatan', 'C5', 0.2, 'Cost');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `penilaian`
+-- Table structure for table `penilaian`
 --
 
-CREATE TABLE IF NOT EXISTS `penilaian` (
+CREATE TABLE `penilaian` (
   `id_penilaian` int(11) NOT NULL,
   `id_alternatif` int(11) NOT NULL,
   `id_kriteria` int(11) NOT NULL,
   `nilai` int(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=128 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `penilaian`
+-- Dumping data for table `penilaian`
 --
 
 INSERT INTO `penilaian` (`id_penilaian`, `id_alternatif`, `id_kriteria`, `nilai`) VALUES
-(103, 16, 24, 84),
-(104, 16, 25, 90),
-(105, 16, 26, 94),
-(106, 16, 27, 100),
-(107, 16, 28, 104),
-(108, 17, 24, 87),
-(109, 17, 25, 92),
-(110, 17, 26, 96),
-(111, 17, 27, 99),
-(112, 17, 28, 106),
-(113, 18, 24, 85),
-(114, 18, 25, 89),
-(115, 18, 26, 98),
-(116, 18, 27, 102),
-(117, 18, 28, 108),
-(118, 19, 24, 88),
-(119, 19, 25, 91),
-(120, 19, 26, 97),
-(121, 19, 27, 101),
-(122, 19, 28, 107),
-(123, 20, 24, 86),
-(124, 20, 25, 93),
-(125, 20, 26, 95),
-(126, 20, 27, 103),
-(127, 20, 28, 105);
+(128, 20, 24, 109),
+(129, 20, 29, 114),
+(130, 20, 30, 117),
+(131, 20, 31, 118),
+(132, 20, 32, 121),
+(133, 21, 24, 109),
+(134, 21, 29, 114),
+(135, 21, 30, 116),
+(136, 21, 31, 119),
+(137, 21, 32, 122),
+(138, 22, 24, 109),
+(139, 22, 29, 114),
+(140, 22, 30, 116),
+(141, 22, 31, 119),
+(142, 22, 32, 121),
+(143, 23, 24, 110),
+(144, 23, 29, 114),
+(145, 23, 30, 117),
+(146, 23, 31, 120),
+(147, 23, 32, 122),
+(148, 24, 24, 110),
+(149, 24, 29, 112),
+(150, 24, 30, 116),
+(151, 24, 31, 119),
+(152, 24, 32, 122),
+(153, 25, 24, 110),
+(154, 25, 29, 114),
+(155, 25, 30, 116),
+(156, 25, 31, 120),
+(157, 25, 32, 123),
+(158, 26, 24, 110),
+(159, 26, 29, 113),
+(160, 26, 30, 117),
+(161, 26, 31, 119),
+(162, 26, 32, 122),
+(163, 27, 24, 110),
+(164, 27, 29, 114),
+(165, 27, 30, 116),
+(166, 27, 31, 118),
+(167, 27, 32, 122),
+(168, 28, 24, 111),
+(169, 28, 29, 114),
+(170, 28, 30, 116),
+(171, 28, 31, 119),
+(172, 28, 32, 122),
+(173, 29, 24, 110),
+(174, 29, 29, 112),
+(175, 29, 30, 116),
+(176, 29, 31, 120),
+(177, 29, 32, 121);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `sub_kriteria`
+-- Table structure for table `sub_kriteria`
 --
 
-CREATE TABLE IF NOT EXISTS `sub_kriteria` (
+CREATE TABLE `sub_kriteria` (
   `id_sub_kriteria` int(11) NOT NULL,
   `id_kriteria` int(11) NOT NULL,
   `deskripsi` varchar(200) NOT NULL,
   `nilai` float NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `sub_kriteria`
+-- Dumping data for table `sub_kriteria`
 --
 
 INSERT INTO `sub_kriteria` (`id_sub_kriteria`, `id_kriteria`, `deskripsi`, `nilai`) VALUES
-(84, 24, '20 Km', 100),
-(85, 24, '30 Km', 80),
-(86, 24, '40 Km', 60),
-(87, 24, '50 Km', 50),
-(88, 24, '60 Km', 40),
-(89, 25, '7', 100),
-(90, 25, '6', 80),
-(91, 25, '5', 60),
-(92, 25, '4', 50),
-(93, 25, '3', 40),
-(94, 26, '7', 100),
-(95, 26, '6', 80),
-(96, 26, '5', 60),
-(97, 26, '4', 50),
-(98, 26, '3', 40),
-(99, 27, '40', 100),
-(100, 27, '36', 80),
-(101, 27, '30', 60),
-(102, 27, '25', 50),
-(103, 27, '20', 40),
-(104, 28, '30', 100),
-(105, 28, '25', 80),
-(106, 28, '20', 60),
-(107, 28, '15', 50),
-(108, 28, '10', 40);
+(109, 24, 'Rendah (Rp 0 - 1.000.000)', 3),
+(110, 24, 'Sedang (Rp 1.000.000 - 2.500.000)', 2),
+(111, 24, 'TInggi (Rp 2.500.000 - keatas', 1),
+(112, 29, 'Tidak Memiliki', 3),
+(113, 29, 'Memiliki Rumah Layak', 1),
+(114, 29, 'Memiliki Rumah Sederhana', 2),
+(115, 30, 'Sedikit (0 - 2)', 1),
+(116, 30, 'Cukup (3 -4)', 2),
+(117, 30, 'Banyak (5 - keatas)', 3),
+(118, 31, 'TIdak Layak Huni', 3),
+(119, 31, 'Layak Huni Sederhana', 2),
+(120, 31, 'Layak Huni', 1),
+(121, 32, 'TIdak Sehat (Penyakit berat, Stroke, Diabetes, dll)', 3),
+(122, 32, 'Sehat Parsial (Penyakit bawaan namun masih bisa melakukan aktivitas)', 2),
+(123, 32, 'Sehat', 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `id_user_level` int(11) NOT NULL,
   `nama` varchar(200) NOT NULL,
   `email` varchar(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_user`, `id_user_level`, `nama`, `email`, `username`, `password`) VALUES
 (1, 1, 'Admin', 'admin@gmail.com', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-(7, 2, 'User', 'user@gmail.com', 'user', 'ee11cbb19052e40b07aac0ca060c23ee');
+(7, 2, 'User', 'user@gmail.com', 'user', 'ee11cbb19052e40b07aac0ca060c23ee'),
+(10, 2, 'warga1', 'warga1@gmail.com', 'warga1', '537aac5fcca2437031413b3309a56c86'),
+(11, 1, 'Septa Puma Surya', 'septapuma@gmail.com', 'rollinpumpkin', '23a1e1f428a8b03bcd8919ee26a1f6df');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user_level`
+-- Table structure for table `user_level`
 --
 
-CREATE TABLE IF NOT EXISTS `user_level` (
+CREATE TABLE `user_level` (
   `id_user_level` int(11) NOT NULL,
   `user_level` varchar(100) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `user_level`
+-- Dumping data for table `user_level`
 --
 
 INSERT INTO `user_level` (`id_user_level`, `user_level`) VALUES
@@ -280,49 +308,56 @@ ALTER TABLE `user_level`
 -- AUTO_INCREMENT for table `alternatif`
 --
 ALTER TABLE `alternatif`
-  MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
+  MODIFY `id_alternatif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
 --
 -- AUTO_INCREMENT for table `hasil`
 --
 ALTER TABLE `hasil`
-  MODIFY `id_hasil` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id_hasil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
 --
 -- AUTO_INCREMENT for table `kriteria`
 --
 ALTER TABLE `kriteria`
-  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+  MODIFY `id_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
 --
 -- AUTO_INCREMENT for table `penilaian`
 --
 ALTER TABLE `penilaian`
-  MODIFY `id_penilaian` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=128;
+  MODIFY `id_penilaian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=178;
+
 --
 -- AUTO_INCREMENT for table `sub_kriteria`
 --
 ALTER TABLE `sub_kriteria`
-  MODIFY `id_sub_kriteria` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=109;
+  MODIFY `id_sub_kriteria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
 --
 -- AUTO_INCREMENT for table `user_level`
 --
 ALTER TABLE `user_level`
-  MODIFY `id_user_level` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id_user_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `hasil`
+-- Constraints for table `hasil`
 --
 ALTER TABLE `hasil`
   ADD CONSTRAINT `hasil_ibfk_1` FOREIGN KEY (`id_alternatif`) REFERENCES `alternatif` (`id_alternatif`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `penilaian`
+-- Constraints for table `penilaian`
 --
 ALTER TABLE `penilaian`
   ADD CONSTRAINT `penilaian_ibfk_1` FOREIGN KEY (`id_alternatif`) REFERENCES `alternatif` (`id_alternatif`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -330,16 +365,17 @@ ALTER TABLE `penilaian`
   ADD CONSTRAINT `penilaian_ibfk_3` FOREIGN KEY (`nilai`) REFERENCES `sub_kriteria` (`id_sub_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `sub_kriteria`
+-- Constraints for table `sub_kriteria`
 --
 ALTER TABLE `sub_kriteria`
   ADD CONSTRAINT `sub_kriteria_ibfk_1` FOREIGN KEY (`id_kriteria`) REFERENCES `kriteria` (`id_kriteria`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `user`
+-- Constraints for table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_user_level`) REFERENCES `user_level` (`id_user_level`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
